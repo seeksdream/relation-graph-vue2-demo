243f95139c826322d537523bcbf24b2fa307d33c
seeks <seeksdream@qq.com>
2023-11-10 21:43:55 +0800

    v2.1.19

--- src/demo/Simple.vue
+++ src/demo/Simple.vue
@@ -10,7 +10,7 @@
     </div>
     <el-button type="success" class="c-show-code-button"
       ><el-link
-        href="https://github.com/seeksdream/relation-graph/blob/master/examples/views/seeks-graph-docs/demo/Demo4Logo.vue"
+        href="https://github.com/seeksdream/relation-graph/blob/master/examples/Demo4Logo.vue"
         target="_blank"
         style="color: #ffffff"
         >查看代码</el-link
@@ -29,25 +29,13 @@ export default {
     return {
       isShowCodePanel: false,
       graphOptions: {
-        debug: true,
-        showDebugPanel: true,
-        backgrounImage: "",
-        downloadImageFileName: "",
-        disableZoom: false,
-        disableDragNode: false,
         moveToCenterWhenRefresh: false,
         zoomToFitWhenRefresh: false,
         useAnimationWhenRefresh: true,
         defaultFocusRootNode: true,
         disableNodeClickEffect: false,
         disableLineClickEffect: false,
-        allowShowZoomMenu: true,
-        allowAutoLayoutIfSupport: true,
-        allowShowRefreshButton: true,
-        allowShowDownloadButton: true,
         backgrounImageNoRepeat: false,
-        allowSwitchLineShape: true,
-        allowSwitchJunctionPoint: true,
         isMoveByParentNode: false,
         defaultExpandHolderPosition: "hide",
         defaultNodeColor: "rgba(238, 178, 94, 1)",
@@ -64,39 +52,11 @@ export default {
         defaultJunctionPoint: "border",
         disableDragCanvas: false,
         lineUseTextPath: false,
-        defaultLineMarker: {
-          markerWidth: 12,
-          markerHeight: 12,
-          refX: 6,
-          refY: 6,
-          data: "M2,2 L10,6 L2,10 L6,6 L2,2",
+        layout:{
+          label: "自动布局",
+          layoutName: "force",
+          layoutClassName: "seeks-layout-force"
         },
-        allowShowSettingPanel: false,
-        allowShowMiniNameFilter: true,
-        checkedNodeId: "N3",
-        checkedLineId: "",
-        layouts: [
-          {
-            label: "自动布局",
-            layoutName: "force",
-            layoutClassName: "seeks-layout-force",
-            useLayoutStyleOptions: false,
-            defaultNodeColor: "#FFC5A6",
-            defaultNodeFontColor: "#000000",
-            defaultNodeBorderColor: "#efefef",
-            defaultNodeBorderWidth: 1,
-            defaultLineColor: "#FD8B37",
-            defaultLineWidth: 1,
-            defaultShowLineLabel: true,
-            defaultLineMarker: {
-              markerWidth: 12,
-              markerHeight: 12,
-              refX: 6,
-              refY: 6,
-              data: "M2,2 L10,6 L2,10 L6,6 L2,2",
-            },
-          },
-        ],
         isNeedShowAutoLayoutButton: true,
         showSingleNode: true,
         showNodeLabel: true,
@@ -109,38 +69,23 @@ export default {
   },
   methods: {
     showSeeksGraph() {
-      // const __graph_json_data = {
-      //   rootId: 'a',
-      //   nodes: [
-      //     { id: 'a', text: 'A', borderColor: 'yellow' },
-      //     { id: 'b', text: 'B', color: '#43a2f1', fontColor: 'yellow' },
-      //     { id: 'c', text: 'C', nodeShape: 1, width: 80, height: 60 },
-      //     { id: 'e', text: 'E', nodeShape: 0, width: 150, height: 150 }
-      //   ],
-      //   lines: [
-      //     { from: 'a', to: 'b', text: '关系1', color: '#43a2f1' },
-      //     { from: 'a', to: 'c', text: '关系2' },
-      //     { from: 'a', to: 'e', text: '关系3' },
-      //     { from: 'b', to: 'e', text: '', color: '#67C23A' }
-      //   ]
-      // };
       const __graph_json_data = {
         rootId: "N3",
         nodes: [
-          { id: "N4", text: "十4" },
-          { id: "N5", text: "十5" },
-          { id: "N6", text: "十6" },
-          { id: "N7", text: "十7" },
-          { id: "N3", text: "十三" },
+          { id: "N4", text: "Node 4" },
+          { id: "N5", text: "Node 5" },
+          { id: "N6", text: "Node 6" },
+          { id: "N7", text: "Node 7" },
+          { id: "N3", text: "Node 三" },
           { id: "N9", text: "152****3393" },
         ],
         lines: [
-          { from: "N3", to: "N9", text: "分享" },
-          { from: "N3", to: "N4", text: "分享444" },
-          { from: "N3", to: "N5", text: "分享555" },
-          { from: "N3", to: "N6", text: "分享666" },
-          { from: "N3", to: "N7", text: "分享777" },
-          { from: "N9", to: "N4", text: "分享x" }
+          { from: "N3", to: "N9", text: "Text 1" },
+          { from: "N3", to: "N4", text: "Text 2" },
+          { from: "N3", to: "N5", text: "Text 3" },
+          { from: "N3", to: "N6", text: "Text 4" },
+          { from: "N3", to: "N7", text: "Text 5" },
+          { from: "N9", to: "N4", text: "Text 6" }
         ],
       };
       this.$refs.seeksRelationGraph.setJsonData(
